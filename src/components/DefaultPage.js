34fbb78bfe550f405eebd110b7c158ed93b53d18
BritneyHanna <b.ahanna@yahoo.com>
2021-09-20 23:02:39 -0400

    changed margins on navbar

--- src/components/DefaultPage.js
+++ src/components/DefaultPage.js
@@ -3,8 +3,18 @@ import React, { Component } from 'react'
 export default class DefaultPage extends Component {
     render() {
         return (
-            <div>
-                <h1> Page Not Found </h1>
+            <div className="row ">
+                <div className="col-12 text-center p-5 mt-5">
+              
+                    
+                <h1 className="display-4"> We're sorry the url: </h1>
+                <h1 className="text-danger display-4">{this.props.location.pathname} </h1>
+                <h1 className="display-4"> was not found </h1>
+
+           
+            
+
+                </div>
             </div>
         )
     }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ function Footer(props) {
         <footer className="site-footer ">
             <div className="container">
                 <div className="row">             
-                    <div className="col-4 col-sm-2 offset-1">
+                    <div className="col-4 ">
                         <h5>Links</h5>
                         <ul className="list-unstyled">
                         <li><Link to='/shop'>Products</Link></li>
@@ -15,14 +15,14 @@ function Footer(props) {
                             <li><Link to='/contactus'>Contact</Link></li>
                         </ul>
                     </div>
-                    <div className="col-6 col-sm-3 text-center">
+                    <div className="col-4  text-center">
                         <h5>Social</h5>
                         <a className="btn btn-social-icon btn-instagram" href="http://instagram.com/"><i className="fa fa-instagram" /></a>{' '}
                         <a className="btn btn-social-icon btn-facebook" href="http://www.facebook.com/"><i className="fa fa-facebook" /></a>{' '}
                         <a className="btn btn-social-icon btn-twitter" href="http://twitter.com/"><i className="fa fa-twitter" /></a>{' '}
                         <a className="btn btn-social-icon btn-google" href="http://youtube.com/"><i className="fa fa-youtube" /></a> 
                     </div>
-                    <div className="col-sm-4 text-center">
+                    <div className=" col-4 text-center">
                         <a role="button" className="btn btn-link" href="tel:+55555555"><i className="fa fa-phone" /> 555-555-5555</a><br />
                         <a role="button" className="btn btn-link" href="mailto:notreal@notreal.co"><i className="fa fa-envelope-o" /> highheels@yahoo.com</a>
                     </div>
